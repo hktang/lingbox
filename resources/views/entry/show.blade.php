2f606d1fe29acb3304698e4e590ebdd5310bcfe0
Xiaojun Deng <xdeng@apn-gcr.org>
2017-06-02 11:44:53 +0900

    Add space for warning-type flash info.

--- resources/views/entry/show.blade.php
+++ resources/views/entry/show.blade.php
@@ -18,7 +18,7 @@
       <div class="col-md-8 col-md-offset-2">
       
           @if ( session('success') )
-            <div class="alert alert-warning .fade .in system-alert">
+            <div class="alert alert-success .fade .in system-alert">
           
                 <span class="glyphicon glyphicon-ok-sign" aria-hidden="true"> </span> 
                 
@@ -27,8 +27,17 @@
                 <a href="#" class="close" data-dismiss="alert" aria-label="close" aria-hidden="true">&times;</a>
                 
             </div>
+          @elseif ( session('warning') )
+            <div class="alert alert-warning .fade .in system-alert">
+          
+                <span class="glyphicon glyphicon-remove-sign" aria-hidden="true"> </span> 
+                
+                {{ session('warning') }}
+
+                <a href="#" class="close" data-dismiss="alert" aria-label="close" aria-hidden="true">&times;</a>
+                
+            </div>
           @endif
-        
       </div>
       
     </div>
