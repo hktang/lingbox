f51f273e4ae7b1c9da49ef138efd5fa29adb3238
Xiaojun Deng <xdeng@apn-gcr.org>
2017-07-11 15:42:11 +0900

    Fix Auth::user() bug.

--- resources/views/entry/singleVotes.blade.php
+++ resources/views/entry/singleVotes.blade.php
@@ -2,19 +2,23 @@
  
  $userDefinitionVoteValue = 0;
 
- $definitionVote = $definition->votes()
-                     ->where('user_id', Auth::user()->id)
-                     ->first();
-
- if( $definitionVote ){
-
-    $userDefinitionVoteValue = $definitionVote->value;
-
- }
-
 @endphp
 
 @if( Auth::user() )
+
+  @php
+
+   $definitionVote = $definition->votes()
+                       ->where('user_id', Auth::user()->id)
+                       ->first();
+
+   if( $definitionVote ){
+
+      $userDefinitionVoteValue = $definitionVote->value;
+
+   }
+
+  @endphp
   
   <a href="#" class="vote vote-up @if($userDefinitionVoteValue == 1) voted @endif" data-id="{{$definition->id}}" data-value="1">
 
